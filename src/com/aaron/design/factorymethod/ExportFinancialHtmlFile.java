788d610d8bef77de10d8552d53178a6f774ce4a3
huangbo <1330195548@qq.com>
2017-06-05 18:58:30 +0800

    update

--- src/com/aaron/design/factorymethod/ExportFinancialHtmlFile.java
+++ src/com/aaron/design/factorymethod/ExportFinancialHtmlFile.java
@@ -2,21 +2,22 @@ package com.aaron.design.factorymethod;
 
 /**
  * 具体导出角色类
+ * 
  * @author Aaron
  * @date 2017年6月5日
  * @version 1.0
  * @package_name com.aaron.design.factorymethod
  */
-public class ExportFinancialHtmlFile implements ExportFile{
+public class ExportFinancialHtmlFile implements ExportFile {
 
 	@Override
 	public boolean export(String data) {
-		 // TODO Auto-generated method stub
-        /**
-         * 业务逻辑
-         */
-        System.out.println("导出财务版HTML文件");
-        return true;
+		// TODO Auto-generated method stub
+		/**
+		 * 业务逻辑
+		 */
+		System.out.println("导出财务版HTML文件");
+		return true;
 	}
 
 }
--- src/com/aaron/design/factorymethod/ExportFinancialPdfFile.java
+++ src/com/aaron/design/factorymethod/ExportFinancialPdfFile.java
@@ -2,21 +2,21 @@ package com.aaron.design.factorymethod;
 
 /**
  * 具体导出角色类
+ * 
  * @author Aaron
  * @date 2017年6月5日
  * @version 1.0
  * @package_name com.aaron.design.factorymethod
  */
-public class ExportFinancialPdfFile implements ExportFile{
+public class ExportFinancialPdfFile implements ExportFile {
 
 	@Override
 	public boolean export(String data) {
-		// TODO Auto-generated method stub
-        /**
-         * 业务逻辑
-         */
-        System.out.println("导出财务版PDF文件");
-        return true;
+		/**
+		 * 业务逻辑
+		 */
+		System.out.println("导出财务版PDF文件");
+		return true;
 	}
 
 }
