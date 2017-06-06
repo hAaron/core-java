e0ce54e957ddf8320e1c68aa5263566ed332eb36
huangbo <1330195548@qq.com>
2017-06-06 16:33:22 +0800

    设计模式--合成模式

---
+++ src/com/aaron/design/composite/security/Component.java
@@ -0,0 +1,16 @@
+package com.aaron.design.composite.security;
+
+/**
+ * 抽象构件角色类
+ * 
+ * @author Aaron
+ * @date 2017年6月6日
+ * @version 1.0
+ * @package_name com.aaron.design.composite.security
+ */
+public interface Component {
+	/**
+	 * 输出组建自身的名称
+	 */
+	public void printStruct(String preStr);
+}
