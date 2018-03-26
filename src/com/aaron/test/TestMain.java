6818d2afe1b346484b55d9b4ae7bd087ddc92279
hAaron <1330195548@qq.com>
2018-03-26 11:03:21 +0800

    测试类提交

---
+++ src/com/aaron/test/TestMain.java
@@ -0,0 +1,17 @@
+package com.aaron.test;
+
+/**
+ * 主测试入口
+ * 
+ * @author Aaron
+ * @date 2018年3月26日
+ * @version 1.0
+ * @package_type com.aaron.test.TestMain
+ */
+public class TestMain {
+	public static void main(String[] args) {
+		System.out.println("主测试入口");
+
+	}
+
+}
---
+++ src/com/aaron/util/memcached/MemcachedUtil.java
@@ -0,0 +1,17 @@
+package com.aaron.util.memcached;
+
+/**
+ * memcached 测试方法
+ * 
+ * @author Aaron
+ * @date 2018年3月26日
+ * @version 1.0
+ * @package_type com.aaron.util.memcached.MemcachedUtil
+ */
+public class MemcachedUtil {
+	public static void main(String[] args) {
+		System.out.println("memcached");
+
+	}
+
+}
