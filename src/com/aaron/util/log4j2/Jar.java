a28f16c66826c68446b7e677b24bf036c6a02fb7
hAaron <1330195548@qq.com>
2018-04-12 10:26:19 +0800

    log4j2日志异步打印测试案例

---
+++ src/com/aaron/util/log4j2/Jar.java
@@ -0,0 +1,67 @@
+package com.aaron.util.log4j2;
+
+/**
+ * log4j2jar包配置
+ * @author Aaron
+ * @date 2018年4月12日
+ * @version 1.0
+ * @package_type com.aaron.util.log4j2.Jar
+ */
+public class Jar {
+	/*<log4j.version>2.8.2</log4j.version>
+	<!-- log4j2 配置 -->
+			<dependency>
+				<groupId>commons-configuration</groupId>
+				<artifactId>commons-configuration</artifactId>
+				<version>1.9</version>
+				<exclusions>
+					<exclusion>
+						<groupId>log4j</groupId>
+						<artifactId>log4j</artifactId>
+					</exclusion>
+				</exclusions>
+			</dependency>
+
+			<dependency>
+				<groupId>org.apache.logging.log4j</groupId>
+				<artifactId>log4j-api</artifactId>
+				<version>${log4j.version}</version>
+			</dependency>
+
+			<dependency>
+				<groupId>org.apache.logging.log4j</groupId>
+				<artifactId>log4j-core</artifactId>
+				<version>${log4j.version}</version>
+			</dependency>
+
+			<dependency>
+				<groupId>org.apache.logging.log4j</groupId>
+				<artifactId>log4j-1.2-api</artifactId>
+				<version>${log4j.version}</version>
+			</dependency>
+
+			<dependency>
+				<groupId>org.apache.logging.log4j</groupId>
+				<artifactId>log4j-slf4j-impl</artifactId>
+				<version>${log4j.version}</version>
+				<exclusions>
+					<exclusion>
+						<groupId>org.slf4j</groupId>
+						<artifactId>slf4j-api</artifactId>
+					</exclusion>
+				</exclusions>
+			</dependency>
+
+			<dependency>
+				<groupId>org.apache.logging.log4j</groupId>
+				<artifactId>log4j-web</artifactId>
+				<version>${log4j.version}</version>
+			</dependency>
+			<!-- 使用异步写日志功能 必须引入此包 -->
+			<dependency>
+				<groupId>com.lmax</groupId>
+				<artifactId>disruptor</artifactId>
+				<version>3.3.6</version>
+			</dependency>
+*/
+}
