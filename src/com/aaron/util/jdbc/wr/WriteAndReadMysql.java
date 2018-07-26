79039fa86d37603552a64fb8db69c54b8159adad
hAaron <1330195548@qq.com>
2018-07-26 09:45:10 +0800

    数据库读写分离

---
+++ src/com/aaron/util/jdbc/wr/WriteAndReadMysql.java
@@ -0,0 +1,42 @@
+package com.aaron.util.jdbc.wr;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import com.aaron.util.jdbc.DBConfig;
+
+/**
+ * 测试数据库读写分离
+ * 
+ * @author Aaron
+ * @date 2018年7月19日
+ * @version 1.0
+ * @package_type com.aaron.util.jdbc.wr.WriteAndReadMysql
+ */
+public class WriteAndReadMysql {
+
+	public static void main(String[] args) {
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		try {
+			String sql = "insert into tb_name(name) values('write4');";
+			// String sqlRead = "select * from tb_name;";
+			connection = DBConfig.getConnection();
+			preparedStatement = connection.prepareStatement(sql);
+			System.out.println(preparedStatement.execute());
+			connection.setAutoCommit(false);
+			connection.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				DBConfig.closeConnection(connection, preparedStatement, resultSet);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
