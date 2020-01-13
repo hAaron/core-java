75d35b3a5b55ee9d414104e2fe14bdbe493ef486
hAaron <1330195548@qq.com>
2020-01-13 15:45:48 +0800

    线程池修改

--- src/com/aaron/Thread/pool/TestThreadPoolExecutor.java
+++ src/com/aaron/Thread/pool/TestThreadPoolExecutor.java
@@ -1,8 +1,6 @@
 package com.aaron.Thread.pool;
 
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -33,7 +31,7 @@ public class TestThreadPoolExecutor {
 		ThreadPoolExecutor threadPool = new ThreadPoolExecutor(2, 4, 5, TimeUnit.SECONDS, queue,
 				new ThreadPoolExecutor.AbortPolicy());
 
-		ExecutorService exec = Executors.newCachedThreadPool();
+		// ExecutorService exec = Executors.newCachedThreadPool();
 
 		// 向线程池里面扔任务
 		for (int i = 0; i < 10; i++) {
