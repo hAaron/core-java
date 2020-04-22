b64708029601e46168dde5ac9f5464582fc665c2
hAaron <1330195548@qq.com>
2020-04-22 17:56:59 +0800

    rabbitmq commit

---
+++ src/com/aaron/util/rabbitmq/quickstart/RabbitMQConfig.java
@@ -0,0 +1,16 @@
+package com.aaron.util.rabbitmq.quickstart;
+
+/**
+ * @author Aaron
+ * @version 1.0
+ * @date 2020/4/22
+ * @package_type com.aaron.util.rabbitmq.quickstart
+ */
+public class RabbitMQConfig {
+  public static final String HOST_NAME = "192.168.150.129";
+  public static final Integer PORT = 5672;
+  public static final String USER = "guest";
+  public static final String PWD = "123456";
+  public static final String QUEUE_NAME = "test";
+  public static final String VIRTUAL_HOST = "/";
+}
