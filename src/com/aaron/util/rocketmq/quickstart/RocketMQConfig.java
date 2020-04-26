080d62d6539941d2a2a8ef1cb6a4add3ab584177
hAaron <1330195548@qq.com>
2020-04-26 22:09:57 +0800

    rocketmq提交

--- src/com/aaron/util/rocketmq/quickstart/RocketMQConfig.java
+++ src/com/aaron/util/rocketmq/quickstart/RocketMQConfig.java
@@ -2,7 +2,7 @@ package com.aaron.util.rocketmq.quickstart;
 
 public class RocketMQConfig {
 
-    public static final String MQ_TOPIC = "TestMQ1";
+    public static final String MQ_TOPIC = "TestMQ2";
     public static final String NAME_ADDR = "3.0.181.3:8276;13.229.201.85:8276";
     public static final String MQ_GROUP = "mqTest";
 
--- src/com/aaron/util/rocketmq/quickstart/RocketMQProcuder.java
+++ src/com/aaron/util/rocketmq/quickstart/RocketMQProcuder.java
@@ -11,26 +11,21 @@ import java.nio.charset.Charset;
 public class RocketMQProcuder {
 
   public static void main(String[] args) {
-      //需要一个producer group名字作为构造方法的参数，这里为producer1
+      //需要一个producer group名字作为构造方法的参数，这里为mqTest
       DefaultMQProducer producer = new DefaultMQProducer("mqTest");
-
       //设置NameServer地址,此处应改为实际NameServer地址，多个地址之间用；分隔
-      //NameServer的地址必须有，但是也可以通过环境变量的方式设置，不一定非得写死在代码里
-      //producer.setNamesrvAddr("3.0.181.3:8276");
       producer.setNamesrvAddr(RocketMQConfig.NAME_ADDR);
       producer.setVipChannelEnabled(false);
-
       //为避免程序启动的时候报错，添加此代码，可以让rocketMq自动创建topickey
-      producer.setCreateTopicKey(RocketMQConfig.MQ_TOPIC);
+      //producer.setCreateTopicKey(RocketMQConfig.MQ_TOPIC);
+      producer.setCreateTopicKey("TBW102");
       try {
           producer.start();
-          for(int i=0;i<10;i++){
+          for(int i=0;i<3;i++){
               try {
-                  Message message = new Message(RocketMQConfig.MQ_TOPIC, "Tag1",
-                          ("Hello RocketMQ " + i).getBytes(Charset.defaultCharset()));
-
+                  Message message = new Message("TestMQ2", "Tag1",
+                          ("TestMQ2 Hello RocketMQ | " + i).getBytes(Charset.defaultCharset()));
                   SendResult sendResult = producer.send(message);
-
                   System.out.println("发送的消息ID:" + sendResult.getMsgId() +"--- 发送消息的状态：" + sendResult.getSendStatus());
               } catch (Exception e) {
                   e.printStackTrace();
@@ -40,7 +35,6 @@ public class RocketMQProcuder {
       } catch (MQClientException | InterruptedException e) {
           e.printStackTrace();
       }
-
       producer.shutdown();
     }
 }
